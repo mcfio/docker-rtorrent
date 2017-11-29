20ed268fa40aba1d9d2e46ec3142497a1313879c
nmcfaul <nmcfaul@users.noreply.github.com>
2017-11-29 00:42:16 -0500

    update entrypoint script to move config files if none exist

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env sh
 set -e
+
+[[ ! -e /root/.rtorrent.rc ]] && cp /defaults/rtorrent.rc /root/.rtorrent.rc
+
+if [ ! -e /config/config.d ]; then
+    mkdir -p /config/config.d
+    cp /defaults/config.d/default.rc /config/config.d/default.rc
+fi
+
+exec /usr/bin/rtorrent

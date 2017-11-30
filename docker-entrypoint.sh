c2d9cabbc564b561bfa4569f45d211039be8668f
nmcfaul <nmcfaul@users.noreply.github.com>
2017-11-29 20:27:42 -0500

    added propert checks for config files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 set -e
 
-[[ ! -e /root/.rtorrent.rc ]] && cp /defaults/rtorrent.rc /root/.rtorrent.rc
+[[ ! -e /root/.rtorrent.rc ]] && \
+    cp /defaults/rtorrent.rc /root/.rtorrent.rc
 
-if [ ! -e /config/config.d ]; then
+[[ ! -e /config/config.d ]] && \
     mkdir -p /config/config.d
+
+[[ ! -e /config/config.d/default.rc ]] && \
     cp /defaults/config.d/default.rc /config/config.d/default.rc
-fi
 
 exec /usr/bin/rtorrent
